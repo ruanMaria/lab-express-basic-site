6664e31263ad26c23f1293373fa3bc2e3d284111
João Maria <joao.ftf.maria@gmail.com>
2020-07-21 18:03:57 +0100

    HBS added

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 const express = require('express')
+const hbs = require('hbs');
 
 const app = express();
 
 app.use(express.static('public'));
 
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+
 
 app.get("/about", (request, response) => {
-    response.sendFile(__dirname + '/views/about.html');
+    response.render('about', {title:'About'})
 });
 app.get("/works", (request, response) => {
-    response.sendFile(__dirname + '/views/works.html');
+    response.render('works', {title:'Works'})
 });
 
 app.get("/gallery", (request, response) => {
-    response.sendFile(__dirname + '/views/gallery.html');
+    response.render('gallery', {title:'Gallery'})
 });
 
 app.get("*", (request, response) => {
     console.log('Request received');
-    response.sendFile(__dirname + '/views/home.html');
+    response.render('home', {title:'Home'})
 
 });
 
